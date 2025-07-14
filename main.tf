1fe2c64c901d52c70acd8d917dec12d5ffe98da0
Kiddo-Sunnu <rakeshsurya2000@gmail.com>
2025-07-14 16:37:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-0454e52560c7f5c55"
+    image_id           = "paste N.VIRGINIA REGION AMI-ID"    <- WRITE THE CHANGES
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t2.micro"
-    key_name = "lastone"
+    key_name = "Rakesh1"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-024286466e258f339", "subnet-09558309acf4661a8"]
+     subnets = ["subnet-paste SUBNET ID1", "subnet-paste SUBNET ID2"]    <- WRITE THE CHANGES go to VPC tab and click on SUBNETS and copy paste the subnet ids here.
      listener {
       instance_port     = 8000
       instance_protocol = "http"
